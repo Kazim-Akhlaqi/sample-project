c868763b4d6865021420b1fa3a5dbfe543ed4ed9
Kazim Akhlaqi <you@example.com>
2018-11-15 11:56:27 -0500

    Update google-fonts.php

--- plugins/google-fonts.php
+++ plugins/google-fonts.php
@@ -1,7 +1,8 @@
 <?php
-/* This plugin to activate google fonts */
+/* plugin to activate google fonts */
 function rmccc_add_fonts(){
     wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?/family=late:400,900');
     wep_enqueue_style('googleFonts');
 }
 add_action('wp_enqueue_scripts', 'rmcc_add_google_fornts');
+?>
