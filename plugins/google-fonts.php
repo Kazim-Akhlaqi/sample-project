bc5a83ffa72bd5bca4a2a791abced2f44ece92be
Kazim Akhlaqi <you@example.com>
2018-11-15 11:54:51 -0500

    Add Sample Project
    Sample Project

---
+++ plugins/google-fonts.php
@@ -0,0 +1,7 @@
+<?php
+/* This plugin to activate google fonts */
+function rmccc_add_fonts(){
+    wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?/family=late:400,900');
+    wep_enqueue_style('googleFonts');
+}
+add_action('wp_enqueue_scripts', 'rmcc_add_google_fornts');
